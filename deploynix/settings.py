0ff67c3bf653994a1f6564258508f6c10cd9b27b
Devi.s <devisanjai2@gmail.com>
2026-08-17 15:10:47 +0530

    Configure email to use TLS on port 587

--- deploynix/settings.py
+++ deploynix/settings.py
@@ -131,13 +131,12 @@ SUBSCRIPTION_ENABLED = False
 # Email configuration (Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', config('EMAIL_PORT', default=587)))
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 
-# Read SMTP sender credentials from Environment Variables
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') or config('EMAIL_HOST_USER', default='')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') or config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', config('EMAIL_HOST_USER', default=''))
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', config('EMAIL_HOST_PASSWORD', default=''))
 
 DEFAULT_FROM_EMAIL = f"Deploynix <{EMAIL_HOST_USER}>"
 SERVER_EMAIL = EMAIL_HOST_USER
